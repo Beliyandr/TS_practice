471357f1f8c4d616c479fae4138d8b608c54c098
beliyandr <beliyandr@gmail.com>
2024-06-12 16:35:40 +0300

    g

--- Dom/index.ts
+++ Dom/index.ts
@@ -6,10 +6,16 @@ const link = document.querySelector("a");
 
 const p = document.querySelector(".paragraph") as HTMLParagraphElement;
 
-const links = document.querySelectorAll("a");
+const links = document.querySelectorAll(".a");
 
 input?.value;
 
 if (link) {
   link.href = "https//google.com";
 }
+
+const elem = document.createElement("a");
+
+link?.addEventListener("scroll", (e) => {
+  e.preventDefault();
+});
---
+++ ts_7_Generic-общение/generic_1.ts
@@ -0,0 +1,5 @@
+function processingData(data: string | number | boolean | string[]) : string | number | boolean | string[]{
+  //....
+
+  return data;
+}
